d3b74402d46d17f334acfaddb8cf347ac369d35d
br0nc080 <mwelbu2@gmail.com>
2012-04-21 23:12:25 -0500

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ring::Application.config.secret_token = '9b0dad98f103f5a60b07ba5aa69d2dc7eff6ab78006f16b1349446b225c7b161bd7a1bbeb6acb06d5224fcabf19700a9ac17614c474a25945d2890c42bfd8fee'
